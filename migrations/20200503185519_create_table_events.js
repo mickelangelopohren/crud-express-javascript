8f21e14362849f57e64ecc0d9f6fd0f260fcf2c7
Mickelangelo Pohren <mickelangelo.ponhren@gmail.com>
2020-05-04 15:01:29 -0300

    Add event presentation date to migration

--- migrations/20200503185519_create_table_events.js
+++ migrations/20200503185519_create_table_events.js
@@ -2,6 +2,7 @@ const up = async (knex) => {
   await knex.schema.createTable('events', (table) => {
     table.bigIncrements('id').unsigned();
     table.string('name', 255).notNullable();
+    table.timestamp('presentation_date').defaultTo(knex.fn.now());
   });
 };
 
