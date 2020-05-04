4be21a3071fa1dc886be1baefdfcc9da19812fb7
Mickelangelo Pohren <mickelangelo.ponhren@gmail.com>
2020-05-04 09:15:36 -0300

    Add database configurations on env file

--- src/env.js
+++ src/env.js
@@ -3,4 +3,11 @@ require('dotenv').config();
 module.exports = {
   /* Application */
   PORT: parseInt(process.env.PORT, 10) || 3000,
+
+  /* Database */
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: parseInt(process.env.DB_PORT, 10),
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
 };
