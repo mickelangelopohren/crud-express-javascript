803adf5a173e54651f1114b449c80916bef6e842
Mickelangelo Pohren <mickelangelo.ponhren@gmail.com>
2020-05-04 10:58:16 -0300

    Add  query logger for env development

--- src/container/index.js
+++ src/container/index.js
@@ -1,7 +1,26 @@
-/** Services */
+const knex = require('knex');
+const knexFile = require('../../knexfile');
+const { logger } = require('../logger');
+const { NODE_ENV } = require('../env');
+
 const eventsService = require('./services/eventsService');
 
-const context = 'tmpString';
+const database = knex(knexFile);
+if (NODE_ENV === 'development') {
+  database.on('query', (query) => {
+    let { sql } = query;
+    if (query.bindings) {
+      query.bindings.forEach((binding) => {
+        sql = sql.replace('?', binding);
+      });
+    }
+    logger.info(sql);
+  });
+}
+
+const context = {
+  database,
+};
 
 module.exports = {
   eventsService: eventsService(context),
